55cc7266028d351b70dd9e64ecf907408a2a65e2
haikalpribadi <haikal@grakn.ai>
2021-03-04 22:59:04 +0000

    Print out to log info

--- concurrent/eventloop/EventLoopExecutor.java
+++ concurrent/eventloop/EventLoopExecutor.java
@@ -117,7 +117,7 @@ public abstract class EventLoopExecutor<E> implements AutoCloseable {
                 Either<Event<E>, Shutdown> event;
                 try {
                     event = queue.take();
-                    System.out.println("Event queue size: " + queue.size());
+                    LOG.info("Event queue size: " + queue.size());
                 } catch (InterruptedException e) {
                     throw GraknException.of(UNEXPECTED_INTERRUPTION);
                 }
